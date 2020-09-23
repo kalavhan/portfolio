6c743645b510748df7283190d213bce7ae54fa4f
kalavhan <josue.brigido1@gmail.com>
2020-09-23 14:23:05 -0600

    Add style to menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 document.getElementById('menuButton').addEventListener('click', () => {
-  alert('jalo');
+  const modal = document.getElementById('menuModal');
+  modal.classList.remove('modal-inactive');
+  modal.classList.add('modal-active');
 })
+
+const closeModal = () => {
+  const modal = document.getElementById('menuModal');
+  modal.classList.remove('modal-active');
+  modal.classList.add('modal-inactive');
+}
