4ecb6c32cbbbef6e9086b9da5fee914f46a88f96
kalavhan <josue.brigido1@gmail.com>
2020-11-18 16:47:33 -0700

    Complete version two

--- mailer.php
+++ mailer.php
@@ -1,11 +1,13 @@
 <?php
   if(isset($_POST['submit'])) {
-    $to_add = "josue.brigido@kalavhan.com"; //<-- put your yahoo/gmail email address here
-    $subject = $_POST['subject'];
+    $name = $_POST['firstName']." ".$_POST['lastName'];
+    $email = $_POST['email'];
+    $to_add = "josue.brigido1@gmail.com"; //<-- put your yahoo/gmail email address here
+    $subject = "Portfolio Kalavhan";
     $message = $_POST['message'];
-    $headers = "From: $_POST['name'] \r\n";
-    $headers .= "Reply-To: $_POST['email'] \r\n";
+    $headers = "From: $name \r\n";
+    $headers .= "Reply-To: $email \r\n";
     $headers .= "X-Mailer: PHP \r\n";
-    mail($to_add,$subject,$message,$headers))
+    mail($to_add,$subject,$message,$headers);
   }
 ?>
